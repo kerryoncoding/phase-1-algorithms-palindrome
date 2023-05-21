736cd1ae2e628ecbe46924d6a8f18733114bb111
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-21 12:02:09 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i=0; i<word.length; i++) {
+    let lettersForward = word[i]
+    let lettersBackward = word[word.length-(i+1)]
+    if ((lettersForward !== lettersBackward)) {
+      return false
+    }
+  } return true
 }
 
 /* 
   Add your pseudocode here
+
+  create function called isPalindrome that takes one argument
+  if (argument is a palidrome) is true, return true
+  else, return false
+
+  to check if arguement is a palindrome
+    loop forward starting with first letter
+    and 
+    loop backwards starting with last letter
+    compare letters.
+    if they do not match... break and return false
+
+    add some examples to test:
+    - dad
+    - poppop
+    - beehive
+
 */
+
 
 /*
   Add written explanation of your solution here
+
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("beehive"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("poppop"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("dad"));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
